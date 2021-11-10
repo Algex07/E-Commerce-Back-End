e65eb7ed2cd3610a3989ce1416296c6c239d9bc3
Amara <amar_algu@hotmail.com>
2021-11-10 03:10:49 -0500

    models created

--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo (Category,{
+  foreignKey: "category_id",
+});
+Category.hasMany(Product,{
+  foreignKey:"category_id",
+});
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongToMany (Tag,{
+  through: {
+  model:  ProductTag,
+  foreignKey: "product_id"
+}
+});
+  
+  Tag.belongToMany(Product, {
+    through:{
+    model: ProductTag,
+     foreignKey:"tag_id"
+    }
+    });
 
 module.exports = {
   Product,
